7dec61bfb8d7997499f21730d5e396f27f14d781
sp905 <31576979+sp905@users.noreply.github.com>
2025-12-16 15:13:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err) => {
